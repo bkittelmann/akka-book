a2bbdbc4b7715678a38f5848c6b671c37cc1a59e
Benno Kittelmann <benno@bitmoving.com>
2014-05-03 17:07:32 +0200

    Test Await.ready with delays in futures causing timeouts

--- src/test/scala/investigation/FuturesActorSpec.scala
+++ src/test/scala/investigation/FuturesActorSpec.scala
@@ -7,11 +7,14 @@ import akka.util.Timeout
 import org.scalatest.{WordSpecLike, Matchers}
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future, Await}
+import scala.util.{Success, Failure}
 
 
 class FuturesActorSpec extends TestKit(ActorSystem()) 
       with WordSpecLike
       with Matchers {
+
+  import ExecutionContext.Implicits.global
 
   "Using futures with actors" should {
 
@@ -26,11 +29,42 @@ class FuturesActorSpec extends TestKit(ActorSystem())
 
       Await.result(f, 1 second) should be ("Echo this back")
     }
+
+    "use different delays" in {
+      val a = system.actorOf(Props[DelayingActor])
+      val futures = (1 to 10) map { i =>
+        val delay = 10 + i * 2
+        val str = s"Delayed for $delay milliseconds"
+        a.ask(DelayedEcho(str, delay))(
+          Timeout((delay * 1.2).toInt)) andThen {
+          case Success(m) => println(s"$m - succeeded")
+          case Failure(e) => println(s"$str - failed")
+        }
+      }
+
+      Await.ready(Future.sequence(futures), 1 second)
+
+      val failed = futures filter { f =>
+        f.value.isEmpty || f.value.get.isFailure
+      }
+
+      println(failed.size + " failed")
+    }
   }
 }
 
 class EchoActor extends Actor {
   def receive = {
     case m => sender ! m
+  }
+}
+
+case class DelayedEcho(msg: String, millis: Long)
+
+class DelayingActor extends Actor {
+  def receive = {
+    case DelayedEcho(msg, millis) =>
+      Thread.sleep(millis)
+      sender ! msg
   }
 }
